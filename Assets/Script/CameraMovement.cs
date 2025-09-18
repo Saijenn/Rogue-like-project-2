9bfcaf724eed476f3ee59d4e0d9c1b7d1fbcbcf2
Saijen <vedricarven@gmail.com>
2025-09-19 00:10:36 +0900

    add temp prefab for platform, camera movement, raycast ground,

---
+++ Assets/Script/CameraMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject player; // Reference to the player object
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,17 +1,21 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
+    private BoxCollider2D boxCollider;
+    [SerializeField]  private LayerMask groundLayer;
     [SerializeField] private float speed = 5.0f;
 
     [SerializeField] private float jumpSpeed = 10.0f;
-   
 
+    
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -26,10 +30,11 @@ public class PlayerMovement : MonoBehaviour
 
 
         //Jump mechanic
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && grounded())
         {
           
             body.linearVelocityY += jumpSpeed; // Add vertical velocity for jumping effect
+            
         }
 
         //Flip player based on movement direction
@@ -39,6 +44,30 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
         }
 
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+     
+        
+    }
+
+    //check if grounded
+    private bool grounded()
+    {
+        //check a little bit under the player if there is ground with raycast
+        RaycastHit2D raycastHit = Physics2D.BoxCast(
+            boxCollider.bounds.center,  // origin (where the box starts from)
+            boxCollider.bounds.size,    // size of the box
+            0f,                         // angle of the box (rotation in degrees)
+            Vector2.down,               // direction to cast (here = downward)
+            0.1f,                       // distance to cast
+            groundLayer                 // which layers to detect
+            );
+
+        
+        return raycastHit.collider != null; //check if we hit something with the raycast (floating in air scan nothing so it's null)
         
     }
 }
