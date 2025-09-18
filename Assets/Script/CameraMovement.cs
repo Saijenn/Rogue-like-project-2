4916902344e138825d63390ae17ba5fd864229e0
Saijen <vedricarven@gmail.com>
2025-09-19 00:34:15 +0900

    changed camera move

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private GameObject player; // Reference to the player object
+    [SerializeField] private Vector3 offset = new Vector3(0f, 2f, 0f); // Offset from the player position
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +14,21 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+       
+    }
+
+    private void LateUpdate()
+    {
+        //smooth camera movement
+        float smoothSpeed = 0.05f; // Adjust this value to change the smoothness of the camera movement
+        Vector3 targetPos = new Vector3(
+         player.transform.position.x + offset.x,
+         player.transform.position.y + offset.y,
+         transform.position.z // <- keep camera's current Z
+        );
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothSpeed);
+
+        //stiff camera movement
+        //transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
     }
 }
